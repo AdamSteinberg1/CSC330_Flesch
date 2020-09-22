3934460cff5af7e2b3dd68579e67ca573bb7e7f8
Adam L. Steinberg <steinberg_al@cobra.cs.mercer.edu>
2020-09-22 10:30:45 -0400

    progress

---
+++ perl/flesch.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Grab the name of the file from the command line, exit if no name given
+my $filename = $ARGV[0] or die "Need to get file name on the  command line\n";
+
+# Use the filename
+open(DATA, "/pub/pounds/CSC330/translations/$filename") or die "Couldn't open file $filename, $!";
+
+#the next line puts all the lines from the text file into an array called @all_lines
+my @all_lines = <DATA>;
+
+#Now take each line and break it into tokens based on spaces and print the token
+foreach my $line (@all_lines) 
+{
+   my @tokens = split(' ', $line);
+   foreach my $token (@tokens) 
+   {
+     print "$token\n";
+   }
+}
+
+sub isWord
+{
+  foreach my $c (@_[0])
+  {
+    if($c)
+  }
+}
