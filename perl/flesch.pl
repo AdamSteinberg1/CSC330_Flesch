68c61a7a4b388c0c6c1694e8be0c530209921f95
Adam L. Steinberg <steinberg_al@gsc_218_3.mercer.edu>
2020-09-22 11:15:57 -0400

    progress

--- perl/flesch.pl
+++ perl/flesch.pl
@@ -11,20 +11,38 @@ open(DATA, "/pub/pounds/CSC330/translations/$filename") or die "Couldn't open fi
 #the next line puts all the lines from the text file into an array called @all_lines
 my @all_lines = <DATA>;
 
+my @words = [];
+
 #Now take each line and break it into tokens based on spaces and print the token
-foreach my $line (@all_lines) 
+foreach my $line (@all_lines)
 {
    my @tokens = split(' ', $line);
-   foreach my $token (@tokens) 
+   foreach my $token (@tokens)
    {
-     print "$token\n";
+     push($token, @words);
    }
+}
+
+foreach my $word (@words)
+{
+  print($word);
 }
 
 sub isWord
 {
   foreach my $c (@_[0])
   {
-    if($c)
+    if($c =~ /[a-zA-Z]/) #if character is a letter'
+    {
+      return true;
+    }
   }
+  return false;
+}
+
+# usage: is_element_of($element,@set);
+# returns 1 if $element is a member of @set
+sub contains {
+    my $item = @_[0];
+    return 1 if grep { $item eq $_ }@_;
 }
